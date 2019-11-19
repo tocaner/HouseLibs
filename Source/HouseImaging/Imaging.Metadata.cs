e1c8113d8db571e9f587797332f384ff446a5259
tocaner <tocaner@gmail.com>
2019-11-18 21:34:02 -0400

    MetadataDefinition, Added property

--- Source/HouseImaging/Imaging.Metadata.cs
+++ Source/HouseImaging/Imaging.Metadata.cs
@@ -516,6 +516,7 @@ namespace HouseImaging
   public class MetadataDefinition
   {
     public int Id { get; set; }
+    public string Path { get; set; }
     public string Category { get; set; }
     public string Name { get; set; }
     public string Description { get; set; }
